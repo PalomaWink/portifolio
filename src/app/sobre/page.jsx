5b0f9386aea23742585349693b24be58c888aa3e
Paloma Wink <paloma.wink@hotmail.com>
2023-11-05 18:10:44 -0300

    Adicionanto texto sobre mim

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -1,5 +1,12 @@
 export default function Sobre() {
   return (
-    <h1>Sobre mim</h1>
+    <div>
+      <h1>Sobre mim</h1>
+      <section>
+        <p>Sou Bióloga formada com uma paixão pela tecnologia que me levou a iniciar uma transição de carreira no início de 2022. Minha jornada começou quando me questionei sobre a profissão que melhor se encaixaria no meu perfil. Sou uma entusiasta do aprendizado, sempre disposta a me desafiar e a adquirir novos conhecimentos.</p>
+        <p>Iniciei meus estudos na Trybe em janeiro de 2023, onde mergulhei em um treinamento abrangente que abordou uma ampla gama de tecnologias, onde tive a oportunidade de desenvolver varios projetos, individuais e em grupo.</p>
+        <p>Estou muito feliz para continuar minha evolução como desenvolvedora full-stack, trazendo minha paixão pela aprendizagem, resolução de problemas e dedicação para contribuir efetivamente em projetos desafiadores. Vamos construir o futuro da tecnologia juntos!</p>
+      </section>
+    </div>
   )
 }
