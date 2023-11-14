31b98c2d3c40dc513c5f6c705cc6dce63cd96ddb
Paloma Wink <paloma.wink@hotmail.com>
2023-11-14 17:02:58 -0300

    Lib para animacao do sobre

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -1,10 +1,28 @@
+"use client"
 import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
+import Typed from 'typed.js';
+import { useEffect, useRef } from "react";
 
 import minhaFoto from '../../../public/minha_foto.jpg';
 
 export default function Sobre() {
+  const el = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Sobre mim...', 'Minha trajetória'],
+      typeSpeed: 150,
+      startDelay: 1000,
+      backSpeed: 150,
+      backDelay: 1000,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, [])
   return (
     <> 
       <Head>
@@ -14,7 +32,11 @@ export default function Sobre() {
       </Head>
       <div className="grid grid-cols-6 mt-10">
         <div className="font-poppins col-span-3 ml-3 flex flex-col justify-center">
-          <h1 className="text-3xl text-center font-bold m-4 tracking-widest">Sobre mim</h1>
+          <h1 className="text-3xl text-center font-bold m-4 tracking-widest text-rose-600">
+            &lt;&nbsp;
+            <span ref={el} />
+            &frasl;&nbsp;&gt;
+          </h1>
           <p className="text-lg text-center">Sou Bióloga formada com uma paixão pela tecnologia que me levou a iniciar uma transição de carreira no início de 2022. Minha jornada começou quando me questionei sobre a profissão que melhor se encaixaria no meu perfil. Sou uma entusiasta do aprendizado, sempre disposta a me desafiar e a adquirir novos conhecimentos.</p>
           <p className="text-lg text-center mt-2">Iniciei meus estudos na Trybe em janeiro de 2023, onde mergulhei em um treinamento abrangente que abordou uma ampla gama de tecnologias, onde tive a oportunidade de desenvolver varios projetos, individuais e em grupo.</p>
           <p className="text-lg text-center mt-2">Estou muito feliz para continuar minha evolução como desenvolvedora full-stack, trazendo minha paixão pela aprendizagem, resolução de problemas e dedicação para contribuir efetivamente em projetos desafiadores. Vamos construir o futuro da tecnologia juntos!</p>
