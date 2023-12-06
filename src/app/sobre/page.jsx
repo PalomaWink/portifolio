e57e88121568bd3c5f60680b76c7d85b385f4e1b
Paloma Wink <paloma.wink@hotmail.com>
2023-12-05 21:21:01 -0300

    add bibloteca AOS

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -5,9 +5,11 @@ import Image from "next/image";
 import Typed from 'typed.js';
 import { useEffect, useRef } from "react";
 import minhaFoto from '../../../public/foto_editada.png';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Sobre() {
-  const el = useRef(null);
+  /* const el = useRef(null);
   const typed = useRef(null);
   useEffect(() => {
     if (el.current) {
@@ -27,6 +29,29 @@ export default function Sobre() {
       }
     };
     
+  }, []) */
+  useEffect(() => {
+    AOS.init({
+      // Global settings:
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+      
+      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 400, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: false, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    
+    });
   }, [])
 
 
@@ -38,7 +63,15 @@ export default function Sobre() {
         <meta name="keywords" content="desenvolvedora, full-stack, tecnologia, projetos" />
       </Head>
       <section className="container mx-auto p-6 min-h-screen font-raleway">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
+        <div
+          className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center"
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-once="false"
+        >
           <div className="order-2 md:order-1">
             <h1 className="text-4xl font-bold text-center md:text-left mb-4 bg-clip-text text-transparent bg-gradient-to-r from-rose-400 to-cyan-400">
             &lt;&nbsp;
@@ -51,7 +84,15 @@ export default function Sobre() {
             <Link href="/">
             </Link>
           </div>
-          <div className="order-1 md:order-2 mb-4 md:mb-0 flex justify-center">
+          <div
+            className="order-1 md:order-2 mb-4 md:mb-0 flex justify-center"
+            data-aos="fade-down"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-once="false"
+          >
             <Image src={minhaFoto} alt="Foto da Paloma" className="w-80 rounded-3xl"  style={{ clipPath: 'circle(75% at center)' }} />
           </div>
         </div>
